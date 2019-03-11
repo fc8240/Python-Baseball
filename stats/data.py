68c94963006838c22770b3ce69b18a98af68acab
chuf <fei.chu@buchanan-edwards.com>
2019-03-10 21:07:17 -0400

    Completed the module

--- stats/data.py
+++ stats/data.py
@@ -21,7 +21,7 @@ identifiers.columns = ['game_id', 'year']
 
 games = pd.concat([games, identifiers], axis=1, sort=False)
 
-games = games.fillna('')
+games = games.fillna(' ')
 
 games.loc[:, 'type'] = pd.Categorical(games.loc[:, 'type'])
 
